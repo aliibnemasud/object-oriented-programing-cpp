df271b57b2c2363490164603de244ec5970f9bc7
Ali Ibne Masud <52651685+aliibnemasud@users.noreply.github.com>
2022-08-27 07:39:51 +0600

    Space updated on the namespace code

--- namespace.cpp
+++ namespace.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
 using namespace std;
-
 namespace first {
-
-    void hello ()
-    {
-        cout << "First Namespace........" << endl;
-    }
+    void hello () { cout << "First Namespace........" << endl; }
     class person {
-
         public:
         string name;
         int age;
-
     };
 }
-
 namespace second {
-
-    void hello ()
-    {
-        cout << "Second Space........" << endl;
-    }
-
+    void hello () { cout << "Second Space........" << endl; }
 }
-
- // using namespace second;
-
-
+// using namespace second
 int main()
-{
-    // cout << "Hello" << endl;
+{    
     second::hello();
-
     first::person obj;
-
     obj.name = "Ali";
     cout << obj.name << endl;
-
-
 }
